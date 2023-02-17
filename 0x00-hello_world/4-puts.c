f7969d325112c86b47a6d4228be46f64823ec13f
Mjadeniyi <Adeboye.mojeed@gmail.com>
2023-02-17 16:53:47 +0000

    Tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-nclude <stdio.h>
-
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
-	    printf("\"Programming is like building a multilingual puzzle");
+	    puts("\"Programming is like building a multilingual puzzle");
+            return (0);
 }
