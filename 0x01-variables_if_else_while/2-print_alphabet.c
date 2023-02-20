d400a6939b3da1df4f3245b994a9686f92f34b86
Mjadeniyi <adeboye.mojeed@gmail.com>
2023-02-20 12:59:45 -0800

    Tasks

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char alphsbet[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alphabet[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	
 	int i;
 	for (i = 0; i < 52; i++)
