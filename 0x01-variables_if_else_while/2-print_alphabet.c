607ede70c108c8ceddc151e68062106d26e4b4b6
Mjadeniyi <adeboye.mojeed@gmail.com>
2023-02-20 12:56:22 -0800

    Tasks

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lowercase alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphsbet[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}i
